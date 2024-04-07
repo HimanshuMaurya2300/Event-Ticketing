58273863216591ec31db2cf7aa6ab513b6747699
Himanshu <mauryah380@gmail.com>
2024-04-07 17:34:11 +0530

    added clerk in webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
 
 export async function POST(req: Request) {
 
@@ -50,22 +50,23 @@ export async function POST(req: Request) {
         })
     }
 
+    // Get the ID and type
     const { id } = evt.data;
     const eventType = evt.type;
 
     if (eventType === 'user.created') {
-        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data
+        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
+            username: username!,
             firstName: first_name,
             lastName: last_name,
             photo: image_url,
-            username: username!
         }
 
-        const newUser = await createUser(user)
+        const newUser = await createUser(user);
 
         if (newUser) {
             await clerkClient.users.updateUserMetadata(id, {
--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default authMiddleware({
         '/api/uploadthing'
     ],
     ignoredRoutes: [
-        '/api/webhook / clerk',
+        '/api/webhook/clerk',
         '/api/webhook/stripe',
         '/api/uploadthing'
     ]
